18983ffc0db1bf8c8924723360f2ce447abd87a0
yogeshkulkarni <yogesh.codeware@gmail.com>
2017-03-21 16:19:18 +0530

    mycommit

--- app/src/main/java/gs/com/mygitdemo/MainActivity.java
+++ app/src/main/java/gs/com/mygitdemo/MainActivity.java
@@ -1,13 +1,33 @@
 package gs.com.mygitdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mbutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mbutButton = (Button) findViewById(R.id.clickbutton);
+        mbutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+
     }
 }
