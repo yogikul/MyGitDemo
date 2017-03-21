f479e8848c0d11181612a843515421af00fb03e6
yogeshkulkarni <yogesh.codeware@gmail.com>
2017-03-21 16:41:00 +0530

    newcommit

--- app/src/main/java/gs/com/mygitdemo/MainActivity.java
+++ app/src/main/java/gs/com/mygitdemo/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///changes
+        ///changes second time changes
 
 
         mbutButton = (Button) findViewById(R.id.clickbutton);
